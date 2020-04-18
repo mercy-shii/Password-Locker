aeb9805a31fbf1f31252e69bea9e3b2e64421fb8
mercy-shii <wemercy525@gmail.com>
2020-04-18 21:39:14 +0300

    changes

--- user.py
+++ user.py
@@ -1,12 +1,12 @@
-class User(Object):
+class User:
     """
     class that generates new instances of users
     """
     User_list = []
 
-    def _init_(self, name, account, passwords):
+    def __init__(self, name, account, passwords):
         self.name = name
         self.account = account
         self.passwords = passwords
 
-pass        
+      
