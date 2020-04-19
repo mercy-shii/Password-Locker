29762c3196cc3e4d47b6278b455131c15d1a6c3a
mercy-shii <wemercy525@gmail.com>
2020-04-20 01:27:26 +0300

    changes

--- credentials.py
+++ credentials.py
@@ -27,6 +27,20 @@ class Credentials:
         delete_credentials method deletes a saved credentials from the credentials_list
         '''
 
-        Credentials.credentials_list.remove(self)    
+        Credentials.credentials_list.remove(self) 
+
+    @classmethod
+    def display_credentials(cls):
+        '''
+        Method that takes in a number and returns a contact that matches that number.
+
+        Args:
+            number: Phone number to search for
+        Returns :
+            Contact of person that matches the number.
+        '''
+
+        
+        return cls.credentials_list      
 
     
