44731e088899ca7a3067c4a63f211e63ea53a5d0
mercy-shii <wemercy525@gmail.com>
2020-04-20 00:12:53 +0300

    changes

---
+++ credentials.py
@@ -0,0 +1,17 @@
+ class Credentials:
+    """
+    Class that generates new instances of credentials
+    """
+
+    credentials_list = [] 
+
+    def __init__(self,username_name,account_name,account_password):
+
+      
+
+        self.username_name = username_name
+        self.account_name = account_name
+        self.account_password = account_password
+        
+
+    
